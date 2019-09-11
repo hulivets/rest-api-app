0b74426507e016e1e4a8627406ca32bb80b6d372
hulivets <48622868+hulivets@users.noreply.github.com>
2019-09-11 05:25:18 +0300

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send("add '/movies' in url, to get movies list")
+    res.send("add '/movies?sort_direction=asc' to url, to get movies list")
 })
 
 module.exports = router
