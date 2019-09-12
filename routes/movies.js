748c1bbd693d56acebed3b772e6413413b036ae5
Hulivets Rostislav <hulivets.rostislav@gmail.com>
2019-09-13 02:42:38 +0300

    add collation

--- routes/movies.js
+++ routes/movies.js
@@ -9,14 +9,14 @@ const upload = multer()
 router.get('/', async (req, res) => {
     try {
         if (req.query.sort_direction === 'asc') {
-            const movies = await Movie.find().sort({ title: 1 })
+            const movies = await Movie.find().collation({ locale: 'en' }).sort({ title: 1 })
             res.status(200).json({
             message: "status ok",
             movies: movies
         })
         } 
         if (req.query.sort_direction === 'desc') {
-            const movies = await Movie.find().sort({ title: -1 })
+            const movies = await Movie.find().collation({ locale: 'en' }).sort({ title: -1 })
             res.status(200).json({
             message: "status ok",
             movies: movies
@@ -81,24 +81,27 @@ module.exports = router
 
 // convert TXT to JSON
 
-// const fs = require('fs')
-// const converter = fs.readFile('sample.txt', (err, data) => {
-//     if (err) throw err
-//     let title = data.toString()
-//     title = title.split(/[\r\n]/)
-//     let txt = []
-//     for (let i = 0; i < title.length; i++) {
-//         if (title[i] === '') continue 
-//             txt.push(title[i])
-//     }
-//     const arr = []
-//     const result = {}
+const fs = require('fs')
+fs.readFile('sample_movies.txt', (err, data) => {
+    // if (err) throw err
+    // let title = data.toString()
+    // title = title.split(/[\r\n]/)
+    // let txt = []
+    // for (let i = 0; i < title.length; i++) {
+    //     if (title[i] === '') continue 
+    //         txt.push(title[i])
+    // }
+    // const arr = []
+    // const result = {}
 
-//     txt.map(line => {
-//         let key = line.split(/[: '']/)[0].toLowerCase()
-//         let value = line.split(':')[1]
-//         result[key] = value
-//         arr.push(result)
+    // txt.map(line => {
+    //     let key = line.split(/[: '']/)[0].toLowerCase()
+    //     let value = line.split(':')[1]
+    //     result[key] = value
+    //     arr.push(result)
 
-//     })
-// })
+    // })
+    let txt = data.toString()
+    
+    console.log(JSON.stringify(txt))
+})
